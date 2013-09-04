18a9bdb727b5bf62dfe98b3b02c1ab786ffca015
Christopher Kruczek <findschgeil@googlemail.com>
2013-09-04 17:33:03 +0200

    updated scheduler to work correctly

--- core/mops_scheduler.s
+++ core/mops_scheduler.s
@@ -9,17 +9,29 @@ mops_resume:
 	stmfd sp!, {r0-r12}
 	// get the address of the threadTableentry we need
 	// on 0x40 we located the table with the addresses of the threads
-	ldr r1, =0x40
+	mov r1,#0x40
+	ldr r1, [r1]
 	// load the currThreadIndex variable
-	ldr r2, =0x50
+	// ****** <begin index>
+	mov r3,#0x50
+	ldr r2, [r3]
+	// check if the index is bigger than 3, than start from beginning and set to 0
+	cmp r2, #2
+	moveq r2, #0
+	streq r2,[r3]
+	// increment and save back
+	add r2,r2, #1
+	str r2,[r3]
+	// ****<end index>
+	
 	// in r0 is the index and we multipli r0 with 4 to get to the next index
 	mov r7,#4
-	mul r0,r0,r7
+	mul r2,r2,r7
 	// finished creating index
-	cmp r0,#0
-	// get the address we need by load the nth-address from r1, index is defined in r0
-	ldrne r0,[r1,r0]
 	
+	// get the address we need by load the nth-address from r1, index is defined in r0
+	mov r1,#0x40
+	ldr r0,[r1,r2]
 	bl 	MOPS_resume
 	
 	.endfunc
